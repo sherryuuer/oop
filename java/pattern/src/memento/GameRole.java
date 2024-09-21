bada1a5276738f371b096253d1fec6ab694e61c9
sherryuuer <sherryuuer@gmail.com>
2024-09-21 17:41:26 +0900

    repairformat

--- java/pattern/src/memento/GameRole.java
+++ java/pattern/src/memento/GameRole.java
@@ -10,46 +10,46 @@ public class GameRole {
     }
 
     public void setVit(int vit) {
-    	this.vit = vit;
+        this.vit = vit;
     }
 
     public int getAtk() {
- 	return atk;   
+        return atk;
     }
 
     public void setAtk(int atk) {
-	this.atk = atk;
+        this.atk = atk;
     }
 
     public int getDef() {
-	return def;
+        return def;
     }
 
     public void setDef(int def) {
-	this.def = def;
+        this.def = def;
     }
 
     // show the state
     public void stateDisplay() {
-	System.out.println("Current state: ");
-	System.out.println("VIT: " + this.vit);
-	System.out.println("ATK: " + this.atk);
-	System.out.println("DEF: " + this.def);
-	System.out.println();
+        System.out.println("Current state: ");
+        System.out.println("VIT: " + this.vit);
+        System.out.println("ATK: " + this.atk);
+        System.out.println("DEF: " + this.def);
+        System.out.println();
     }
 
     // get the init state
     public void getInitState() {
-	this.vit = 100;
-	this.atk = 100;
-	this.def = 100;
+        this.vit = 100;
+        this.atk = 100;
+        this.def = 100;
     }
 
     // fight
     public void fight() {
-	this.vit = 0;
-	this.atk = 0;
-	this.def = 0;
+        this.vit = 0;
+        this.atk = 0;
+        this.def = 0;
     }
 
     // save the state to memento
@@ -59,9 +59,8 @@ public class GameRole {
 
     // recovery the role state
     public void recoveryState(RoleStateMemento memento) {
-	this.vit = memento.getVit();
-	this.atk = memento.getAtk();
-	this.def = memento.getDef();
+        this.vit = memento.getVit();
+        this.atk = memento.getAtk();
+        this.def = memento.getDef();
     }
 }
-
