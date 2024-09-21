e8d47bca21b73df1f2e8179855b2305ea13850f9
Sallyw <41533280+sherryuuer@users.noreply.github.com>
2024-09-21 12:49:21 +0900

    Update GameRole.java

--- java/pattern/src/memento/GameRole.java
+++ java/pattern/src/memento/GameRole.java
@@ -1,8 +1,8 @@
 package memento;
 
 public class GameRole {
-	private int vit;
-	private int atk;
+    private int vit;
+    private int atk;
     private int def;
 
     public int getVit() {
@@ -13,63 +13,55 @@ public class GameRole {
     	this.vit = vit;
     }
 
-	public int getAtk() {
- 		return atk;   
-	}
+    public int getAtk() {
+ 	return atk;   
+    }
 
-	public void setAtk(int atk) {
-		this.atk = atk;
-	}
+    public void setAtk(int atk) {
+	this.atk = atk;
+    }
 
-	public int getDef() {
-		return def;
-	}
+    public int getDef() {
+	return def;
+    }
 
-	public void setDef(int def) {
-		this.def = def;
-	}
+    public void setDef(int def) {
+	this.def = def;
+    }
 
-	// show the state
-	public void stateDisplay() {
-		System.out.println("Current state: ");
-		System.out.println("VIT: " + this.vit);
-		System.out.println("ATK: " + this.atk);
-		System.out.println("DEF: " + this.def);
-		System.out.println();
-	}
+    // show the state
+    public void stateDisplay() {
+	System.out.println("Current state: ");
+	System.out.println("VIT: " + this.vit);
+	System.out.println("ATK: " + this.atk);
+	System.out.println("DEF: " + this.def);
+	System.out.println();
+    }
 
-	// get the init state
-	public void getInitState() {
-		this.vit = 100;
-		this.atk = 100;
-		this.def = 100;
-	}
+    // get the init state
+    public void getInitState() {
+	this.vit = 100;
+	this.atk = 100;
+	this.def = 100;
+    }
 
-	// fight
-	public void fight() {
-		this.vit = 0;
-		this.atk = 0;
-		this.def = 0;
-	}
+    // fight
+    public void fight() {
+	this.vit = 0;
+	this.atk = 0;
+	this.def = 0;
+    }
 
-	// save the state to memento
-	public RoleStateMemento saveState() {
-		return new RoleStateMemento(vit, atk, def)
-	}
+    // save the state to memento
+    public RoleStateMemento saveState() {
+	return new RoleStateMemento(vit, atk, def)
+    }
 
-	// recovery the role state
-	public void recoveryState(RoleStateMemento memento) {
-		this.vit = memento.getVit();
-		this.atk = memento.getAtk();
-		this.def = memento.getDef();
-	}
+    // recovery the role state
+    public void recoveryState(RoleStateMemento memento) {
+	this.vit = memento.getVit();
+	this.atk = memento.getAtk();
+	this.def = memento.getDef();
+    }
 }
-
-
-
-
-
-
-
-
 
